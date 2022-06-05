61ff07b01ec27c1879226807f8c33ae10c38a57c
AfnanFerdousi <afnanferdousi550@gmail.com>
2022-06-05 20:17:44 +0600

    homepage cards

--- src/Components/Home/Hero.js
+++ src/Components/Home/Hero.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import home1 from "../../images and icons/home 1st image.png"
-import home2 from "../../images and icons/home 2nd image.png"
-import home3 from "../../images and icons/home 3rd image.png"
+import home1 from "../../images and icons/home 1st image.png";
+import home2 from "../../images and icons/home 2nd image.png";
+import home3 from "../../images and icons/home 3rd image.png";
+import Girl from "../../images and icons/girl.png";
 const Hero = () => {
     return (
         <div class="hero min-h-screen">
             <div class="hero-content grid lg:grid-cols-2 sm:grid-cols-1">
                 <div className="flex flex-col">
-                    <img src={home1} alt="" />
+                    <div className='flex'>
+                        <img src={Girl} alt="" />
+                        <div className='bg-[#202020] py-4 px-6 h-[100%] mt-[10px] 
+                        ml-[-35px] flex'>
+                            <div>
+                                <h2 className='text-[26px] text-[#FFFFFF]'>Tell us what u need</h2>
+                                <p className='text-[12px] text-[#FFFFFF]'>Whatever the service you're looking for, we'll help you find a professional for the job.</p>
+                            </div>
+                            <h1 className='text-[114px] text-[#484848] mt-[-20px] 
+                            mb-[-70px] mr-[-15px]'>1</h1>
+                        </div>
+                    </div>
+                    {/* <img src={home1} alt="" /> */}
                     <img src={home2} alt="" />
                     <img src={home3} alt="" />
                 </div>
